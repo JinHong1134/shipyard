273dedffcfe8854fc0defb5e3a53c00431b6e166
黄文禹 <whoami@huangwenyudeMacBook-Pro.local>
2019-08-20 22:11:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+船厂管理系统
+spring boot: 2.1.7
+日志框架: Logback
+JDK: 1.8
+持久化框架: MyBatis
+IDE: IntelliJ IDEA
---
+++ src/main/java/com/hwy/shipyard/ShipyardApplication.java
@@ -0,0 +1,13 @@
+package com.hwy.shipyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipyardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipyardApplication.class, args);
+    }
+
+}
