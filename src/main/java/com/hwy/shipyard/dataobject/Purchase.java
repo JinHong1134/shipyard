a9d849a730da3732933aaa89e4fe472306b791ec
黄文禹 <whoami@huangwenyudeMacBook-Pro.local>
2019-08-21 16:14:56 +0800

    update pom.xml

--- src/main/java/com/hwy/shipyard/dataobject/Purchase.java
+++ src/main/java/com/hwy/shipyard/dataobject/Purchase.java
@@ -1,4 +1,74 @@
 package com.hwy.shipyard.dataobject;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 采购单
+ */
+@Data
 public class Purchase {
+
+    /**
+     * 采购单编号，主键
+     */
+    private String purchaseId;
+
+    /**
+     *供货商
+     */
+    private String factoryName;
+
+    /**
+     * 订货日期
+     */
+    private Date purchaseCreationTime;
+
+    /**
+     * 到货日期
+     */
+    private Date purchaseDeliveryTime;
+
+    /**
+     * 订单总价
+     */
+    private BigDecimal purchaseTotalPrice;
+
+    /**
+     * 业务员
+     */
+    private String operatorName;
+
+    /**
+     * 校验位
+     */
+    private String checkBits;
+
+    /**
+     * 上一条记录的哈希
+     */
+    private String purchasePre;
+
+    /**
+     * 预留字段
+     */
+    private String purchaseField0;
+
+    /**
+     * 预留字段
+     */
+    private String purchaseField1;
+
+    /**
+     * 预留字段
+     */
+    private String purchaseField2;
+
+    /**
+     * 预留字段
+     */
+    private String purchaseField3;
+
 }
--- src/main/java/com/hwy/shipyard/mapper/PurchaseMapper.java
+++ src/main/java/com/hwy/shipyard/mapper/PurchaseMapper.java
@@ -1,4 +1,39 @@
 package com.hwy.shipyard.mapper;
 
-public class PurchaseMapper {
+import com.github.pagehelper.Page;
+import com.hwy.shipyard.dataobject.Purchase;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+
+
+/**
+ * 订单的dao
+ */
+
+public interface PurchaseMapper {
+
+    @Select("select * from purchase where purchase_id = #{purchaseId}")
+    @Results(value = {
+            @Result(column = "purchase_id", property = "purchaseId"),
+            @Result(column = "factory_name", property = "factoryName"),
+            @Result(column = "purchase_creation_time", property = "purchaseCreationTime"),
+            @Result(column = "purchase_delivery_time", property = "purchaseDeliveryTime"),
+            @Result(column = "purchase_total_price", property = "purchaseTotalPrice"),
+            @Result(column = "operator_name", property = "operatorName"),
+            @Result(column = "check_bits", property = "checkBits"),
+            @Result(column = "purchase_pre", property = "purchasePre"),
+            @Result(column = "purchase_field0", property = "purchaseField0"),
+            @Result(column = "purchase_field1", property = "purchaseField1"),
+            @Result(column = "purchase_field2", property = "purchaseField2"),
+            @Result(column = "purchase_field3", property = "purchaseField3")
+    })
+    Purchase findById(String purchaseId);
+
+
+    Page<Purchase> findByFactoryName(String factoryName);
+
+
+
 }
